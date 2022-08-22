1b839c17ab7336c737ea84b1a63c2f475fed4e1f
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-08-22 23:45:52 +0600

    player adding-js added

---
+++ js/player-addin.js
@@ -0,0 +1,30 @@
+const selectArray = [];
+
+const selectBtnAll = document.getElementsByClassName('btn-select');
+
+for(const selectBtn of selectBtnAll){
+    selectBtn.addEventListener('click', function(event){
+        const selectElement = event.target.parentNode.children[1].innerText;
+        const selectedPlayer = document.getElementById('selected-Player');
+        if(selectArray.length < 5){
+            selectArray.push(selectElement);
+            const playerOl = document.getElementById('player-ol');
+            const li = document.createElement('li');
+            li.innerText = `${selectElement}`
+            playerOl.appendChild(li);
+            selectedPlayer.innerText = selectArray.length;
+            selectBtn.setAttribute('disabled', '');
+            selectBtn.style.backgroundColor = '#81e6d9';
+            selectBtn.style.color = 'black';
+        } else {
+            alert('You Cannot Select More Than Five Player!!!')
+            return;
+        }
+        // console.log(selectArray.length);
+        // console.log(selectArray)
+        // const playerOl = document.getElementById('player-ol');
+        // const li = document.createElement('li');
+        // li.innerText = `${selectElement}`
+        // playerOl.appendChild(li);
+    })
+}
